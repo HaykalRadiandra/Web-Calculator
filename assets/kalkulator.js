27a03f4e6dabd79b5b4c0707ba3e16646717bb64
HaykalRadiandra <rthaykal@gmail.com>
2022-07-28 02:24:55 +0700

    kosong

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -44,21 +44,6 @@ function handleOperator (operator) {
   }
 }
 
-function performCalculation() {
-  if (calculator.firstNumber == null || calculator.operator == null) {
-    alert('Anda belum menetapkan operator');
-    return;
-  }
- 
-  let result = 0;
-  if (calculator.operator === '+') {
-    result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-  } else {
-    result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
-  }
- 
-  calculator.displayNumber = result;
-}
 
 const buttons = document.querySelectorAll('.button');
 for (const button of buttons) {
