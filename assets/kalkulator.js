c4d7ebf1084eb20acd9eead8dcc3acee9314396d
HaykalRadiandra <rthaykal@gmail.com>
2022-07-28 01:46:13 +0700

    menambahkan fungsi event pada class clear

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -29,6 +29,12 @@ for (const button of buttons) {
   button.addEventListener('click', function (event) {
     // mendapatkan objek elemen yang diklik
     const target = event.target;
+
+    if (target.classList.contains('clear')) {
+      clearCalculator();
+      updateDisplay();
+      return
+    }
     
     inputDigit(target.innerText);
     updateDisplay();
